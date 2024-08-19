8b8a59ab7aefc516b256f4d65ba48e8c11966958
kartik0603 <kartikhirapara800@gmail.com>
2024-08-19 20:38:54 +0530

    NODE EXAM 3

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,4 +12,16 @@ const userController = {
     
 };
 
+// login
+const loggedIn ={
+    async login(req, res) {
+        try {
+            const user = await User.findOne(req.body);
+            res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
+
 module.exports = userController;
