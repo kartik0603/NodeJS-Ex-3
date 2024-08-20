bef1bb544b5eec2c7b4d018d37a0c24022404361
kartik0603 <kartikhirapara800@gmail.com>
2024-08-20 11:50:48 +0530

    NODE EXAM 3

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
-const express =require("express");
-const dbConnect=require("./config/db");
-const userRouter =require("./routes/user.route")
-const cors =require("cors");
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
-app.get("/", (req, res)=>{
-    res.send({messaage :"Started"})
-    
-})
+// FIlE Creation
 
-app.use("/user",userRouter);
+app.post("/file/create", (req, res) => {
+  const { name, content } = req.body;
 
-app.listen(8090, ()=>{
-    console.log("Server is running on port 8090")
-    dbConnect();
-})
+  fs.appendFile(name, content, (err) => {
+    if (err) {
+      res.send({ message: "Error" });
+    } else {
+      res.send({ message: "File Created" });
+    }
+  });
+});
+
+app.put("/file/update", (req, res) => {
+  const { name, content } = req.body;
+  fs.writeFile(name, content, (err) => {
+    if (err) {
+      res.send({ message: "Error" });
+    } else {
+      res.send({ message: "File Updated" });
+    }
+  });
+});
+
+app.delete("/file/delete", (req, res) => {
+  const { name } = req.body;
+  fs.unlink(name, (err) => {
+    if (err) {
+      res.send({ message: "Error" });
+    } else {
+      res.send({ message: "File Deleted" });
+    }
+  });
+});
+
+// CAlculator
+
+app.post("/cal/sum", (req, res) => {
+  const { numbers } = req.body;
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  res.send({ sum });
+});
+
+app.post("/cal/sub", (req, res) => {
+  const { numbers } = req.body;
+  const sub = numbers.reduce((acc, curr) => acc - curr, 0);
+  res.send({ sub });
+});
+
+app.post("/cal/mul", (req, res) => {
+  const { numbers } = req.body;
+  const mul = numbers.reduce((acc, curr) => acc * curr, 1);
+  res.send({ mul });
+});
+
+app.post("/cal/div", (req, res) => {
+  const { numbers } = req.body;
+  if (numbers === 0) {
+    res.send({ message: "Error" });
+  } else {
+    const divi = numbers.reduce((acc, curr) => acc / curr, 1);
+    res.send({ divi });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send({ messaage: "Started" });
+});
+
+app.listen(8090, () => {
+  console.log("Server is running on port 8090");
+});
--- routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { createUser } = require('../controllers/user.controller');
-
-const userRouter = Router();
-
-userRouter.post('/signup', createUser);
-
-module.exports = userRouter;
